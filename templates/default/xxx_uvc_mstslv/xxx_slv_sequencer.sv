03f472448760f927cebd1e03a398530002a94c6c
HolmeXin2630 <hx1076260997@outlook.com>
2026-06-17 01:28:21 +0800

    feat: add mstslv slave templates (agent, driver, monitor, sequencer)

---
+++ templates/default/xxx_uvc_mstslv/xxx_slv_sequencer.sv
@@ -0,0 +1,18 @@
+`ifndef {{ uvc_info.uvc_name.upper() }}_SLV_SEQUENCER__SV
+`define {{ uvc_info.uvc_name.upper() }}_SLV_SEQUENCER__SV
+
+class {{ uvc_info.uvc_name }}_slv_sequencer extends uvm_sequencer #({{ uvc_info.uvc_name }}_transaction);
+    {{ uvc_info.uvc_name }}_cfg cfg;
+
+    `uvm_component_utils({{ uvc_info.uvc_name }}_slv_sequencer)
+
+    function new(string name,uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+    endfunction: build_phase
+endclass
+
+`endif //{{ uvc_info.uvc_name.upper() }}_SLV_SEQUENCER__SV
